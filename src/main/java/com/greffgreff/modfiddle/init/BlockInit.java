b5800294fd0fedada89ba109c1f609c9f5ba9a86
chandlergreff <I433816@fhict.nl>
2022-01-16 22:10:21 +0100

    cleaned class

--- src/main/java/com/greffgreff/modfiddle/init/BlockInit.java
+++ src/main/java/com/greffgreff/modfiddle/init/BlockInit.java
@@ -8,12 +8,14 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class BlockInit {
+    // Setup
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, ModFiddle.MOD_ID);
-
-    public static final RegistryObject<Block> MYBLOCK = registerBlock("my_block", new MyBlock());
 
     public static RegistryObject<Block> registerBlock(String blockName, Block block) {
         ItemInit.registerBlockItem(blockName, block);
         return BLOCKS.register(blockName, () -> block);
     }
+
+    // Blocks
+    public static final RegistryObject<Block> MYBLOCK = registerBlock("my_block", new MyBlock());
 }
