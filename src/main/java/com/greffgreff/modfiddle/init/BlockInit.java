c0d55a021f6b57583d29584418cd98e128ca7a46
chandlergreff <I433816@fhict.nl>
2022-01-16 22:09:39 +0100

    cleaned imports on block init

--- src/main/java/com/greffgreff/modfiddle/init/BlockInit.java
+++ src/main/java/com/greffgreff/modfiddle/init/BlockInit.java
@@ -1,13 +1,8 @@
-package com.greffgreff.modfiddle.core.init;
+package com.greffgreff.modfiddle.init;
 
 import com.greffgreff.modfiddle.ModFiddle;
 import com.greffgreff.modfiddle._blocks.MyBlock;
-import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
-import net.minecraft.block.SoundType;
-import net.minecraft.block.material.Material;
-import net.minecraft.block.material.MaterialColor;
-import net.minecraftforge.common.ToolType;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
