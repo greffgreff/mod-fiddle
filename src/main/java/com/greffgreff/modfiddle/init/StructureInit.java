23f5db99f184a9528b69bb6206742f3da0836916
chandlergreff <I433816@fhict.nl>
2022-01-19 02:44:37 +0100

    added rarity and spacing of structure as method

--- src/main/java/com/greffgreff/modfiddle/init/StructureInit.java
+++ src/main/java/com/greffgreff/modfiddle/init/StructureInit.java
@@ -1,17 +1,93 @@
 package com.greffgreff.modfiddle.init;
 
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 import com.greffgreff.modfiddle.ModFiddle;
 import com.greffgreff.modfiddle.world.structure.structures.TotemStructure;
+import net.minecraft.util.registry.WorldGenRegistries;
 import net.minecraft.world.gen.feature.NoFeatureConfig;
 import net.minecraft.world.gen.feature.structure.Structure;
+import net.minecraft.world.gen.settings.DimensionStructuresSettings;
+import net.minecraft.world.gen.settings.StructureSeparationSettings;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class StructureInit {
     // Setup
     public static final DeferredRegister<Structure<?>> STRUCTURES = DeferredRegister.create(ForgeRegistries.STRUCTURE_FEATURES, ModFiddle.MOD_ID);
 
+    public static <F extends Structure<?>> void setupMapSpacingAndLand(F structure, StructureSeparationSettings structureSeparationSettings, boolean transformSurroundingLand) {
+        // adding structure to vanilla structures
+        Structure.NAME_STRUCTURE_BIMAP.put(structure.getRegistryName().toString(), structure);
+
+        /*
+         * Whether surrounding land will be modified automatically to conform to the bottom of the structure.
+         * Basically, it adds land at the base of the structure like it does for Villages and Outposts.
+         * Doesn't work well on structure that have pieces stacked vertically or change in heights.
+         * Note: The air space this method will create will be filled with water if the structure is below sealevel.
+         * This means this is best for structure above sealevel so keep that in mind.
+         */
+        if (transformSurroundingLand) {
+            Structure.field_236384_t_ = ImmutableList.<Structure<?>>builder()
+                    .addAll(Structure.field_236384_t_)
+                    .add(structure)
+                    .build();
+        }
+
+        /*
+         * This is the map that holds the default spacing of all structures.
+         * Always add your structure to here so that other mods can utilize it if needed.
+         *
+         * However, while it does propagate the spacing to some correct dimensions from this map,
+         * it seems it doesn't always work for code made dimensions as they read from this list beforehand.
+         *
+         * Instead, we will use the WorldEvent.Load event in StructureTutorialMain to add the structure
+         * spacing from this list into that dimension or to do dimension blacklisting properly.
+         * We also use our entry in DimensionStructuresSettings.DEFAULTS in WorldEvent.Load as well.
+         */
+        DimensionStructuresSettings.field_236191_b_ = ImmutableMap.<Structure<?>, StructureSeparationSettings>builder()
+                .putAll(DimensionStructuresSettings.field_236191_b_)
+                .put(structure, structureSeparationSettings)
+                .build();
+
+        /*
+         * There are very few mods that relies on seeing your structure in the noise settings registry before the world is made.
+         * You may see some mods add their spacings to DimensionSettings.BUILTIN_OVERWORLD instead of the NOISE_GENERATOR_SETTINGS loop below but
+         * that field only applies for the default overworld and won't add to other worldtypes or dimensions (like amplified or Nether).
+         * So yeah, don't do DimensionSettings.BUILTIN_OVERWORLD. Use the NOISE_GENERATOR_SETTINGS loop below instead if you must.
+         */
+        WorldGenRegistries.NOISE_SETTINGS.getEntries().forEach(settings -> {
+            Map<Structure<?>, StructureSeparationSettings> structureMap = settings.getValue().getStructures().func_236195_a_();
+
+            /*
+             * Pre-caution in case a mod makes the structure map immutable like datapacks do.
+             * I take no chances myself. You never know what another mods does...
+             */
+            if (structureMap instanceof ImmutableMap) {
+                Map<Structure<?>, StructureSeparationSettings> tempMap = new HashMap<>(structureMap);
+                tempMap.put(structure, structureSeparationSettings);
+                settings.getValue().getStructures().func_236195_a_();
+            }
+            else {
+                structureMap.put(structure, structureSeparationSettings);
+            }
+        });
+    }
+
     // Structures
     public static final RegistryObject<Structure<NoFeatureConfig>> TOTEM_STRUCTURE = STRUCTURES.register("totem", TotemStructure::new);
+
+    public static void setupStructures() {
+        // p_i232019_1_ = avg dist apart between attempts
+        // p_i232019_2_ = minimum distance apart in chunks between spawn attempts (< avg dist)
+        // p_i232019_3_ = unique seed to avoid two structures overlapping
+        StructureSeparationSettings settings =  new StructureSeparationSettings(2, 1 , 1234563890 );
+        setupMapSpacingAndLand(TOTEM_STRUCTURE.get(), settings, true);
+
+        // Add more structures here and so on
+    }
 }
