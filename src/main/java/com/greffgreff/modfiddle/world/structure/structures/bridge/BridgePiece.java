19052237a289694a7062ef14ef48708ed78c5d3b
chandlergreff <I433816@fhict.nl>
2022-02-01 20:54:31 +0100

    added bridge piece class

---
+++ src/main/java/com/greffgreff/modfiddle/world/structure/structures/bridge/BridgePiece.java
@@ -0,0 +1,136 @@
+package com.greffgreff.modfiddle.world.structure.structures.bridge;
+
+import net.minecraft.world.gen.feature.structure.StructurePiece;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+public class BridgePiece implements List<StructurePiece> {
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public Iterator<StructurePiece> iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        return null;
+    }
+
+    @Override
+    public boolean add(StructurePiece piece) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends StructurePiece> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends StructurePiece> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
+
+    @Override
+    public StructurePiece get(int index) {
+        return null;
+    }
+
+    @Override
+    public StructurePiece set(int index, StructurePiece element) {
+        return null;
+    }
+
+    @Override
+    public void add(int index, StructurePiece element) {
+
+    }
+
+    @Override
+    public StructurePiece remove(int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public ListIterator<StructurePiece> listIterator() {
+        return null;
+    }
+
+    @Override
+    public ListIterator<StructurePiece> listIterator(int index) {
+        return null;
+    }
+
+    @Override
+    public List<StructurePiece> subList(int fromIndex, int toIndex) {
+        return null;
+    }
+}
