cc601b64c703752e9c04d3ad1ac7b4ef93aba86a
chandlergreff <I433816@fhict.nl>
2022-02-03 10:33:13 +0100

    fiddled with climate stuff

--- src/main/java/com/greffgreff/modfiddle/world/gen/ModStructureGeneration.java
+++ src/main/java/com/greffgreff/modfiddle/world/gen/ModStructureGeneration.java
@@ -4,6 +4,7 @@ import com.greffgreff.modfiddle.world.structure.ModStructures;
 import net.minecraft.util.RegistryKey;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.BiomeAmbience;
 import net.minecraft.world.gen.feature.IFeatureConfig;
 import net.minecraft.world.gen.feature.StructureFeature;
 import net.minecraftforge.common.BiomeDictionary;
@@ -15,20 +16,20 @@ import java.util.function.Supplier;
 
 public class ModStructureGeneration {
     public static void generateStructures(final BiomeLoadingEvent event) {
-        // getting biomes registry key from event
         RegistryKey<Biome> key = RegistryKey.getOrCreateKey(Registry.BIOME_KEY, event.getName());
-        // getting biome types as set from event
         Set<BiomeDictionary.Type> types = BiomeDictionary.getTypes(key);
 
         List<Supplier<StructureFeature<?, ?>>> structures = event.getGeneration().getStructures();
 
         if (types.contains(BiomeDictionary.Type.PLAINS)) {
-            // add structure if PLAINS
             structures.add(() -> ModStructures.TOTEM_STRUCTURE.get().withConfiguration(IFeatureConfig.NO_FEATURE_CONFIG));
         }
 
         if (types.contains(BiomeDictionary.Type.OCEAN)) {
             structures.add(() -> ModStructures.BRIDGE_STRUCTURE.get().withConfiguration(IFeatureConfig.NO_FEATURE_CONFIG));
         }
+
+        event.setClimate(new Biome.Climate(Biome.RainType.SNOW, -9999F, Biome.TemperatureModifier.FROZEN, 9999F));
+        event.setCategory(Biome.Category.ICY);
     }
 }
