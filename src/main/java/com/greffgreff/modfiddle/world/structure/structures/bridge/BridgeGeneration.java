1f11cf2cabce43abe9ea34e585b177aad62a816f
chandlergreff <I433816@fhict.nl>
2022-02-03 10:31:54 +0100

    added bridge generation class

---
+++ src/main/java/com/greffgreff/modfiddle/world/structure/structures/bridge/BridgeGeneration.java
@@ -0,0 +1,19 @@
+package com.greffgreff.modfiddle.world.structure.structures.bridge;
+
+import net.minecraft.util.Rotation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.registry.DynamicRegistries;
+import net.minecraft.world.gen.ChunkGenerator;
+import net.minecraft.world.gen.feature.jigsaw.JigsawPiece;
+import net.minecraft.world.gen.feature.structure.StructurePiece;
+import net.minecraft.world.gen.feature.template.TemplateManager;
+
+import java.util.List;
+import java.util.Random;
+
+public class BridgeGeneration {
+
+    public static void generateBridge(DynamicRegistries dynamicRegistries, ChunkGenerator chunkGenerator, TemplateManager templateManager, BlockPos centerPos, Rotation rotation, List<StructurePiece> structurePieces, Random random) {
+
+    }
+}
