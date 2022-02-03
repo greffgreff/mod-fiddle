1a32b87c03cf6776bb928aa15695d8f445a92498
chandlergreff <I433816@fhict.nl>
2022-02-03 14:00:46 +0100

    removed structure pieces  from params in AbstractBridgePiece

--- src/main/java/com/greffgreff/modfiddle/world/structure/structures/bridge/BridgeGeneration.java
+++ src/main/java/com/greffgreff/modfiddle/world/structure/structures/bridge/BridgeGeneration.java
@@ -1,10 +1,11 @@
 package com.greffgreff.modfiddle.world.structure.structures.bridge;
 
-import net.minecraft.util.Rotation;
+import com.greffgreff.modfiddle.ModFiddle;
+import com.greffgreff.modfiddle.world.structure.structures.bridge.pieces.TowerPiece;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.registry.DynamicRegistries;
 import net.minecraft.world.gen.ChunkGenerator;
-import net.minecraft.world.gen.feature.jigsaw.JigsawPiece;
 import net.minecraft.world.gen.feature.structure.StructurePiece;
 import net.minecraft.world.gen.feature.template.TemplateManager;
 
@@ -13,7 +14,11 @@ import java.util.Random;
 
 public class BridgeGeneration {
 
-    public static void generateBridge(DynamicRegistries dynamicRegistries, ChunkGenerator chunkGenerator, TemplateManager templateManager, BlockPos centerPos, Rotation rotation, List<StructurePiece> structurePieces, Random random) {
+    public static void generateBridge(DynamicRegistries dynamicRegistries, ChunkGenerator chunkGenerator, TemplateManager templateManager, BlockPos centerPos, List<StructurePiece> structurePieces, Random random) {
+        ResourceLocation loc = new ResourceLocation(ModFiddle.MOD_ID, "bridge/bridge");
+        TowerPiece piece = new TowerPiece(dynamicRegistries, chunkGenerator, templateManager, centerPos, random, loc);
+        piece.createPiece();
+
 
     }
 }
