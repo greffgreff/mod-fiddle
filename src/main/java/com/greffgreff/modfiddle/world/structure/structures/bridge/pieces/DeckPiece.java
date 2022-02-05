2408f408d45b27a49801476faf1ac91982122dd0
chandlergreff <I433816@fhict.nl>
2022-02-05 12:11:46 +0100

    made use of super joinJigsaw

--- src/main/java/com/greffgreff/modfiddle/world/structure/structures/bridge/pieces/DeckPiece.java
+++ src/main/java/com/greffgreff/modfiddle/world/structure/structures/bridge/pieces/DeckPiece.java
@@ -1,5 +1,6 @@
 package com.greffgreff.modfiddle.world.structure.structures.bridge.pieces;
 
+import com.greffgreff.modfiddle.ModFiddle;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.Rotation;
 import net.minecraft.util.math.BlockPos;
@@ -7,6 +8,7 @@ import net.minecraft.util.math.MutableBoundingBox;
 import net.minecraft.util.registry.DynamicRegistries;
 import net.minecraft.world.gen.ChunkGenerator;
 import net.minecraft.world.gen.feature.jigsaw.JigsawPiece;
+import net.minecraft.world.gen.feature.structure.AbstractVillagePiece;
 import net.minecraft.world.gen.feature.structure.StructurePiece;
 import net.minecraft.world.gen.feature.template.TemplateManager;
 
@@ -16,26 +18,38 @@ import java.util.Random;
 public class DeckPiece extends AbstractBridgePiece {
     public final int deckLength;
 
-    public DeckPiece(DynamicRegistries dynamicRegistries, ChunkGenerator chunkGenerator, TemplateManager templateManager, BlockPos position, Random random, ResourceLocation poolLocation, int deckLength) {
-        super(dynamicRegistries, chunkGenerator, templateManager, position, random, poolLocation);
+    public DeckPiece(DynamicRegistries dynamicRegistries, ChunkGenerator chunkGenerator, TemplateManager templateManager, BlockPos position, Rotation rotation, Random random, ResourceLocation poolLocation, int deckLength) {
+        super(dynamicRegistries, chunkGenerator, templateManager, position, rotation, random, poolLocation);
         this.deckLength = deckLength;
     }
 
-    public DeckPiece(DynamicRegistries dynamicRegistries, ChunkGenerator chunkGenerator, TemplateManager templateManager, BlockPos position, Random random, ResourceLocation poolLocation) {
-        this(dynamicRegistries, chunkGenerator, templateManager, position, random, poolLocation, 3);
+    public DeckPiece(DynamicRegistries dynamicRegistries, ChunkGenerator chunkGenerator, TemplateManager templateManager, BlockPos position, Rotation rotation, Random random, ResourceLocation poolLocation) {
+        this(dynamicRegistries, chunkGenerator, templateManager, position, rotation, random, poolLocation, 3);
     }
 
     @Override
     public List<StructurePiece> createPiece() {
+//        for (int i = 0; i < deckLength; i++) {
+//            JigsawPiece deckPiece = getRandomDeckPiece();
+//            if (structurePieces.isEmpty()) {
+//                structurePieces.add(createAbstractPiece(deckPiece, position, rotation));
+//            }
+//            else {
+//                MutableBoundingBox prevPieceBB = structurePieces.get(i-1).getBoundingBox();
+//                BlockPos deckPos = new BlockPos(position.getX(), position.getY(), prevPieceBB.minZ + prevPieceBB.getZSize());
+//                structurePieces.add(createAbstractPiece(deckPiece, deckPos, rotation));
+//            }
+//        }
+
         for (int i = 0; i < deckLength; i++) {
             JigsawPiece deckPiece = getRandomDeckPiece();
             if (structurePieces.isEmpty()) {
-                structurePieces.add(createAbstractPiece(deckPiece, position, Rotation.NONE));
+                structurePieces.add(createAbstractPiece(deckPiece, position, rotation));
             }
             else {
-                MutableBoundingBox prevPieceBB = structurePieces.get(i-1).getBoundingBox();
-                BlockPos deckPos = new BlockPos(position.getX(), position.getY(), prevPieceBB.minZ + prevPieceBB.getZSize());
-                structurePieces.add(createAbstractPiece(deckPiece, deckPos, Rotation.NONE));
+                AbstractVillagePiece deckPiecePlaced = createAbstractPiece(deckPiece, position, rotation);
+                joinJigsaws((AbstractVillagePiece) structurePieces.get(i-1), deckPiecePlaced);
+                structurePieces.add(deckPiecePlaced);
             }
         }
 
