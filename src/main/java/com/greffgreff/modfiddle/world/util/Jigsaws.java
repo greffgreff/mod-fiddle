14896e454edd6b7328dad3ea139d6dcdc3042229
chandlergreff <I433816@fhict.nl>
2022-02-06 13:28:02 +0100

    added Jigsaws util class

---
+++ src/main/java/com/greffgreff/modfiddle/world/util/Jigsaws.java
@@ -0,0 +1,32 @@
+package com.greffgreff.modfiddle.world.util;
+
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.Rotation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.registry.DynamicRegistries;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.world.gen.feature.jigsaw.JigsawPattern;
+import net.minecraft.world.gen.feature.jigsaw.JigsawPiece;
+import net.minecraft.world.gen.feature.jigsaw.SingleJigsawPiece;
+import net.minecraft.world.gen.feature.structure.AbstractVillagePiece;
+import net.minecraft.world.gen.feature.template.TemplateManager;
+
+import java.util.function.Supplier;
+
+public class Jigsaws {
+
+    public static AbstractVillagePiece createAbstractPiece(JigsawPiece piece, BlockPos position, Rotation rotation, TemplateManager templateManager) {
+        return new AbstractVillagePiece(templateManager, piece, position, piece.getGroundLevelDelta(), rotation, piece.getBoundingBox(templateManager, position, rotation));
+    }
+
+    public static JigsawPattern getPool(ResourceLocation resourceLocation, DynamicRegistries dynamicRegistries) {
+        Supplier<JigsawPattern> piecesPool = () -> dynamicRegistries.getRegistry(Registry.JIGSAW_POOL_KEY).getOrDefault(resourceLocation);
+        return piecesPool.get();
+    }
+
+    public static String getPieceName(JigsawPiece jigsawPiece) {
+        if (jigsawPiece == null) return "";
+        SingleJigsawPiece singleJigsawPiece = (SingleJigsawPiece) jigsawPiece;
+        return singleJigsawPiece.field_236839_c_.left().isPresent() ? singleJigsawPiece.field_236839_c_.left().get().getPath() : "";
+    }
+}
